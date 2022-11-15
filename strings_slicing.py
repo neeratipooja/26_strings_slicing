94e5e1369f29a2eaef96b883e1a383ba0d6bb3ac
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-11-15 22:47:11 +0530

    Add files via upload

---
+++ strings_slicing.py
@@ -0,0 +1,21 @@
+'''
+DATE=15TH NOV 2022
+DAY= TUESDAY
+TOPIC= STRINGS_SLICING
+AUTHOR= POOJA
+'''
+a='python'
+print(a,type(a)) #python str
+print(a[0]) #p
+print('python'[0]) #p
+print('python'[1]) #y
+print('python'[2]) #t
+print('python'[3]) #h
+print('python'[4]) #o
+print('python'[5]) #n
+b=['python','c','c++']
+print(type(b)) #list
+print(b[0][5]) #n
+print(b[0])  #python
+print(b[1]) #c
+print(b[2]) #c++
